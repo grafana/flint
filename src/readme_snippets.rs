a54bc616f69c0279bee2a737c272d4873621fe07
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2026-05-09 16:57:49 +0000

    chore: remove obsolete README renovate sync Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- src/readme_snippets.rs
+++
@@ -1,4 +0,0 @@
-pub const INSTALL_MARKER: &str = "Add `flint` to your repo's `mise.toml`:";
-pub const QUICKSTART_MARKER: &str =
-    "Add the linting tools your project needs alongside the `flint` binary itself:";
-pub const QUICKSTART_KEYS: &[&str] = &["shellcheck", "shfmt", "actionlint"];
